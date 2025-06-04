a63bfccfde46c1988b869078b42f1578fce1319c
Bensagra <bensagra@gmail.com>
2025-06-04 09:03:39 -0300

    Remove temporary debug files and caches for Assembly-CSharp and Assembly-CSharp-Editor projects to clean up the build environment. This includes deletion of various .assets.cache, .csproj.nuget.dgspec.json, .csproj.nuget.g.props, .csproj.nuget.g.targets, and project.assets.json files, ensuring a fresh state for future builds.

---
+++ Assets/Scripts/DineroManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class DineroManager : MonoBehaviour
+{
+    public float playerMoney;
+
+    public bool UpdateMoney(float amount)
+    {
+
+        if (playerMoney + amount < 0)
+        {
+            return false;
+        }
+        else
+        {
+            playerMoney += amount;
+            return true;
+
+        }
+   }
+}
